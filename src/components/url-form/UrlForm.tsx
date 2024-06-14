abf31898641576d1dcad39e8b30d40f5884c765f
Ryan <ryanbprog@gmail.com>
2024-06-14 17:28:01 +0800

    styling complete

--- src/components/url-form/UrlForm.tsx
+++ src/components/url-form/UrlForm.tsx
@@ -2,9 +2,14 @@ import styles from "./UrlForm.module.scss";
 
 export default function UrlForm() {
   return (
-    <form action="" className={styles.form}>
-      <input type="text" />
-      <button>Shorten It!</button>
-    </form>
+    <section className={styles.urlForm}>
+      <form action="">
+        <input type="text" placeholder="Shorten a link here..." />
+        <button>Shorten It!</button>
+        <span>
+          <i>Please add a link</i>
+        </span>
+      </form>
+    </section>
   );
 }
