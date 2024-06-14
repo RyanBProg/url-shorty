d3ec6ea6c916126eedcc7f6534f2d7c3e4d62419
Ryan <ryanbprog@gmail.com>
2024-06-14 16:11:48 +0800

    hero styling complete

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,5 +1,19 @@
 import styles from "./Hero.module.scss";
 
 export default function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className={styles.hero}>
+      <div>
+        <h1>More than just shorter links</h1>
+        <p>
+          Build your brand's recognition and get detailed insights on how your
+          links are performing
+        </p>
+        <button>Get Started</button>
+      </div>
+      <div>
+        <img src="/images/illustration-working.svg" alt="background image" />
+      </div>
+    </section>
+  );
 }
